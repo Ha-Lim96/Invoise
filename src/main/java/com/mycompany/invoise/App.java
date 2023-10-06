4a04f8c225821f43b9682e5541c52645c78e4785
Ha-Lim96 <ouladmansour.adelhalim@gmail.com>
2023-10-06 02:31:34 +0200

    illustration

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -1,5 +1,11 @@
 package com.mycompany.invoise;
 
+import com.mycompany.invoise.entity.Invoice;
+import com.mycompany.invoise.service.InvoiceService;
+import com.sun.tools.jdeprscan.scan.Scan;
+
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "What is the customerName ?" );
+        Scanner sc = new Scanner(System.in);
+        String customerName = sc.nextLine();
+
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(customerName);
+
+        InvoiceService invoiceService = new InvoiceService();
+        invoiceService.createInvoice(invoice);
     }
 }
+s
