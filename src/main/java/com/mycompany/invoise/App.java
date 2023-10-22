9f55e1b35d7a3fc9c239af3e3d227a7c611a72b4
Ha-Lim96 <ouladmansour.adelhalim@gmail.com>
2023-10-22 23:01:56 +0200

    Affectation d'une valeur à une propriete de type file

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -1,16 +1,9 @@
 package com.mycompany.invoise;
 
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
-
-import com.mycompany.invoise.controller.InvoiceControllerWeb;
-import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
-
-import com.mycompany.invoise.service.InvoiceServiceInterface;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.Scanner;
 
 
 public class App 
@@ -22,3 +15,5 @@ public class App
 
     }
 }
+
+
--- src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
+++ src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
@@ -3,10 +3,32 @@ package com.mycompany.invoise.service;
 import com.mycompany.invoise.entity.Invoice;
 import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
 
+import java.io.File;
+import java.util.Date;
+
 public class InvoiceServicePrefix implements InvoiceServiceInterface {
 
-    private static long lastNumber = 0L;
+    private  long lastNumber = 112L;
+    private String prefix;
     private InvoiceRepositoryInterface invoiceRepository;
+    private File fichier;
+
+
+    public long getLastNumber() {
+        return lastNumber;
+    }
+
+    public void setLastNumber(long lastNumber) {
+        this.lastNumber = lastNumber;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
 
     public InvoiceRepositoryInterface getInvoiceRepository() {
         return invoiceRepository;
@@ -16,8 +38,9 @@ public class InvoiceServicePrefix implements InvoiceServiceInterface {
         this.invoiceRepository = invoiceRepository;
     }
 
+
     public void createInvoice(Invoice invoice){
-        invoice.setNumber(String.valueOf("INV_"+(++lastNumber)));
+        invoice.setNumber(String.valueOf(prefix+(++lastNumber)));
         invoiceRepository.create(invoice);
     }
 }
