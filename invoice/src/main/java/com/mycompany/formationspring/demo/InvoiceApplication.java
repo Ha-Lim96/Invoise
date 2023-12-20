e3f7d9912562cff8b03bea8926a6b7968b190a58
Ha-Lim96 <ouladmansour.adelhalim@gmail.com>
2023-12-20 02:02:39 +0100

    Découpage en microservices

---
+++ invoice/src/main/java/com/mycompany/formationspring/demo/InvoiceApplication.java
@@ -0,0 +1,21 @@
+package com.mycompany.formationspring.demo;
+
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceApplication.class, args);
+	}
+
+
+	@Bean
+	public Hibernate5Module datatypeHibernateModule() {
+		return new Hibernate5Module();
+	}
+
+}
