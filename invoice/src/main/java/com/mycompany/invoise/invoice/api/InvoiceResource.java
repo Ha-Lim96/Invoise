376a49f60640a8e59d2efbbe0284aa8a880e0ace
Ha-Lim96 <ouladmansour.adelhalim@gmail.com>
2023-12-26 19:06:24 +0100

    Obtenir l'emplacement d'un microservice @LoadBalanced

--- invoice/src/main/java/com/mycompany/invoise/invoice/api/InvoiceResource.java
+++ invoice/src/main/java/com/mycompany/invoise/invoice/api/InvoiceResource.java
@@ -38,17 +38,18 @@ public class InvoiceResource {
         System.out.println("La méthode displayHome a été appelé");
         Iterable<Invoice> invoices = invoiceService.getInvoiceList();
         invoices.forEach(invoice-> {
-            invoice.setCustomer(restTemplate.getForObject("http://localhost:8083/customer/"+invoice.getIdCustomer(), Customer.class));
+            invoice.setCustomer(restTemplate.getForObject("http://customer-service/customer/"+invoice.getIdCustomer(), Customer.class));
         });
         return invoices;
     }
+
 
     @GetMapping("/{id}")
     public Invoice get(@PathVariable("id")String number){
         System.out.println("La méthode displayInvoice a été invoqué");
         Invoice invoice = invoiceService.getInvoiceByNumber(number);
-        final Customer customer = restTemplate.getForObject("http://localhost:8083/customer/"+invoice.getIdCustomer(), Customer.class);
-        final Address address = restTemplate.getForObject("http://localhost:8083/address/"+customer.getAddress().getId(), Address.class);
+        final Customer customer = restTemplate.getForObject("http://customer-service/customer/"+invoice.getIdCustomer(), Customer.class);
+        final Address address = restTemplate.getForObject("http://customer-service/address/"+customer.getAddress().getId(), Address.class);
         customer.setAddress(address);
         invoice.setCustomer(customer);
         return invoice;
